4178e207881ac2426b5f90500937f2f7563ff19e
samepant <sampanter@gmail.com>
2021-03-10 22:17:55 -0500

    use await for faunadb call in lambda

--- lambda/create-multisig.js
+++ lambda/create-multisig.js
@@ -15,23 +15,17 @@ exports.handler = (event, context, callback) => {
   const multisig = {
     data: data,
   };
-  /* construct the fauna query */
-  return client
-    .query(q.Create(q.Collection("Multisig"), multisig))
-    .then((response) => {
-      console.log("success", response);
-      /* Success! return the response with statusCode 200 */
-      return callback(null, {
-        statusCode: 200,
-        body: JSON.stringify(response),
-      });
-    })
-    .catch((error) => {
-      console.log("error", error);
-      /* Error! return the error with statusCode 400 */
-      return callback(null, {
-        statusCode: 400,
-        body: JSON.stringify(error),
-      });
-    });
+  try {
+    const faunaRes = await client.query(q.Create(q.Collection("Multisig"), multisig))
+    console.log("success", response);
+    return {
+      statusCode: 200,
+      body: JSON.stringify(response),
+    }
+  } catch (error) {
+    return { 
+      statusCode: 400,
+      body: JSON.stringify(error),
+    }
+  }
 };
