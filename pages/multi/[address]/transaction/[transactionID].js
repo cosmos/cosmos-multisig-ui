88e76ed8dc420b78b76a08b078b903220ea278b3
samepant <sampanter@gmail.com>
2021-08-29 18:12:29 -0400

    force deploy

--- pages/multi/[address]/transaction/[transactionID].js
+++ pages/multi/[address]/transaction/[transactionID].js
@@ -157,7 +157,7 @@ const transactionPage = ({
           border-radius: 10px;
           text-align: center;
           font-family: monospace;
-          max-width: 476px;
+          max-width: 475px;
         }
       `}</style>
     </Page>
