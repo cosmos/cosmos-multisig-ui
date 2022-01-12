579f5893db6fa1088df7b23517129917eb6e21bf
Simon Warta <simon@warta.it>
2022-01-12 23:14:00 +0100

    Improve eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,4 @@
+/* global module */
 module.exports = {
   env: {
     es6: true,
@@ -8,23 +9,33 @@ module.exports = {
   },
   parserOptions: {
     sourceType: "module",
-    ecmaVersion: 8,
+    ecmaVersion: 2020,
     ecmaFeatures: {
       jsx: true,
     },
   },
   plugins: ["prettier"],
-  extends: ["eslint:recommended", "prettier", "plugin:prettier/recommended"],
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "prettier",
+    "plugin:prettier/recommended",
+  ],
   rules: {
     curly: ["warn", "multi-line", "consistent"],
     "no-bitwise": "warn",
     "no-console": "off",
     "no-param-reassign": "warn",
     "no-shadow": "warn",
-    "no-unused-vars": "warn",
+    "no-unused-vars": [
+      "warn",
+      { argsIgnorePattern: "^_", varsIgnorePattern: "^_" },
+    ],
     "prefer-const": "warn",
     radix: ["warn", "always"],
     "spaced-comment": ["warn", "always"],
+    "react/no-unescaped-entities": ["warn", { forbid: [">", "}"] }], // by default we can't use ' which is annoying
+    "react/prop-types": "off", // we take care of this with TypeScript
   },
   overrides: [],
 };
