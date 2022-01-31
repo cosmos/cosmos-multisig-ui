2f3033635c45d4007b444891fdac514844fb16ce
Simon Warta <simon@warta.it>
2022-01-31 16:15:25 +0100

    Backport NEXT_PUBLIC_CHAIN_DISPLAY_NAME from #24

--- pages/index.js
+++ pages/index.js
@@ -4,11 +4,13 @@ import FindMultisigForm from "../components/forms/FindMultisigForm";
 import Page from "../components/layout/Page";
 import StackableContainer from "../components/layout/StackableContainer";
 
+const chainDisplayName = process.env.NEXT_PUBLIC_CHAIN_DISPLAY_NAME;
+
 const MultiPage = () => (
   <Page>
     <StackableContainer base>
       <StackableContainer lessPadding>
-        <h1 className="title">Cosmoshub Multisig Manager</h1>
+        <h1 className="title">{chainDisplayName} Multisig Manager</h1>
       </StackableContainer>
       <FindMultisigForm />
     </StackableContainer>
