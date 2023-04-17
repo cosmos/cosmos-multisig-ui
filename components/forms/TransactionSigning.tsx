4448a31cf9ae34f872c48dd6baf4789acccd5bd3
abefernan <44572727+abefernan@users.noreply.github.com>
2023-04-17 16:50:42 +0200

    Add loading states to connect and sign form

--- components/forms/TransactionSigning.tsx
+++ components/forms/TransactionSigning.tsx
@@ -1,18 +1,22 @@
-import React, { useState } from "react";
-import axios from "axios";
+import { makeCosmoshubPath } from "@cosmjs/amino";
 import { toBase64 } from "@cosmjs/encoding";
+import { LedgerSigner } from "@cosmjs/ledger-amino";
 import { SigningStargateClient } from "@cosmjs/stargate";
 import { assert } from "@cosmjs/utils";
-
-import { useAppContext } from "../../context/AppContext";
-import Button from "../inputs/Button";
-import HashView from "../dataViews/HashView";
-import StackableContainer from "../layout/StackableContainer";
-import { DbSignature, DbTransaction, WalletAccount } from "../../types";
-
 import TransportWebUSB from "@ledgerhq/hw-transport-webusb";
-import { LedgerSigner } from "@cosmjs/ledger-amino";
-import { makeCosmoshubPath } from "@cosmjs/amino";
+import axios from "axios";
+import { useState } from "react";
+import { useAppContext } from "../../context/AppContext";
+import { DbSignature, DbTransaction, WalletAccount } from "../../types";
+import HashView from "../dataViews/HashView";
+import Button from "../inputs/Button";
+import StackableContainer from "../layout/StackableContainer";
+
+interface LoadingStates {
+  readonly signing?: boolean;
+  readonly keplr?: boolean;
+  readonly ledger?: boolean;
+}
 
 interface Props {
   signatures: DbSignature[];
@@ -28,10 +32,13 @@ const TransactionSigning = (props: Props) => {
   const [hasSigned, setHasSigned] = useState(false);
   const [walletType, setWalletType] = useState<"Keplr" | "Ledger">();
   const [ledgerSigner, setLedgerSigner] = useState({});
+  const [loading, setLoading] = useState<LoadingStates>({});
 
   const connectKeplr = async () => {
     try {
       assert(state.chain.chainId, "chainId missing");
+
+      setLoading((newLoading) => ({ ...newLoading, keplr: true }));
       await window.keplr.enable(state.chain.chainId);
       window.keplr.defaultOptions = {
         sign: { preferNoSetFee: true, preferNoSetMemo: true, disableBalanceCheck: true },
@@ -45,81 +52,102 @@ const TransactionSigning = (props: Props) => {
       setHasSigned(tempHasSigned);
       setWalletType("Keplr");
     } catch (e) {
-      console.log("enable err: ", e);
+      console.log("enable keplr err: ", e);
+    } finally {
+      setLoading((newLoading) => ({ ...newLoading, keplr: false }));
     }
   };
 
   const connectLedger = async () => {
-    assert(state.chain.addressPrefix, "addressPrefix missing");
+    try {
+      assert(state.chain.addressPrefix, "addressPrefix missing");
 
-    // Prepare ledger
-    const ledgerTransport = await TransportWebUSB.create(120000, 120000);
+      setLoading((newLoading) => ({ ...newLoading, ledger: true }));
+      // Prepare ledger
+      const ledgerTransport = await TransportWebUSB.create(120000, 120000);
 
-    // Setup signer
-    const offlineSigner = new LedgerSigner(ledgerTransport, {
-      hdPaths: [makeCosmoshubPath(0)],
-      prefix: state.chain.addressPrefix,
-    });
-    console.log(offlineSigner);
-    const accounts = await offlineSigner.getAccounts();
-    console.log(accounts);
-    const tempWalletAccount: WalletAccount = {
-      bech32Address: accounts[0].address,
-      pubkey: accounts[0].pubkey,
-      algo: accounts[0].algo,
-    };
+      // Setup signer
+      const offlineSigner = new LedgerSigner(ledgerTransport, {
+        hdPaths: [makeCosmoshubPath(0)],
+        prefix: state.chain.addressPrefix,
+      });
+      console.log(offlineSigner);
+      const accounts = await offlineSigner.getAccounts();
+      console.log(accounts);
+      const tempWalletAccount: WalletAccount = {
+        bech32Address: accounts[0].address,
+        pubkey: accounts[0].pubkey,
+        algo: accounts[0].algo,
+      };
 
-    const tempHasSigned = props.signatures.some(
-      (sig) => sig.address === tempWalletAccount.bech32Address,
-    );
-    setWalletAccount(tempWalletAccount);
-    setHasSigned(tempHasSigned);
-    setLedgerSigner(offlineSigner);
-    setWalletType("Ledger");
+      const tempHasSigned = props.signatures.some(
+        (sig) => sig.address === tempWalletAccount.bech32Address,
+      );
+      setWalletAccount(tempWalletAccount);
+      setHasSigned(tempHasSigned);
+      setLedgerSigner(offlineSigner);
+      setWalletType("Ledger");
+    } catch (e) {
+      console.log("enable ledger err: ", e);
+    } finally {
+      setLoading((newLoading) => ({ ...newLoading, ledger: false }));
+    }
   };
 
   const signTransaction = async () => {
     assert(state.chain.chainId, "chainId missing");
 
-    const offlineSigner =
-      walletType === "Keplr" ? window.getOfflineSignerOnlyAmino(state.chain.chainId) : ledgerSigner;
+    try {
+      setLoading((newLoading) => ({ ...newLoading, signing: true }));
+      const offlineSigner =
+        walletType === "Keplr"
+          ? window.getOfflineSignerOnlyAmino(state.chain.chainId)
+          : ledgerSigner;
 
-    const signerAddress = walletAccount?.bech32Address;
-    assert(signerAddress, "Missing signer address");
-    const signingClient = await SigningStargateClient.offline(offlineSigner);
+      const signerAddress = walletAccount?.bech32Address;
+      assert(signerAddress, "Missing signer address");
+      const signingClient = await SigningStargateClient.offline(offlineSigner);
 
-    const signerData = {
-      accountNumber: props.tx.accountNumber,
-      sequence: props.tx.sequence,
-      chainId: state.chain.chainId,
-    };
-
-    const { bodyBytes, signatures } = await signingClient.sign(
-      signerAddress,
-      props.tx.msgs,
-      props.tx.fee,
-      props.tx.memo,
-      signerData,
-    );
-
-    // check existing signatures
-    const bases64EncodedSignature = toBase64(signatures[0]);
-    const bases64EncodedBodyBytes = toBase64(bodyBytes);
-    const prevSigMatch = props.signatures.findIndex(
-      (signature) => signature.signature === bases64EncodedSignature,
-    );
-
-    if (prevSigMatch > -1) {
-      setSigError("This account has already signed.");
-    } else {
-      const signature = {
-        bodyBytes: bases64EncodedBodyBytes,
-        signature: bases64EncodedSignature,
-        address: signerAddress,
+      const signerData = {
+        accountNumber: props.tx.accountNumber,
+        sequence: props.tx.sequence,
+        chainId: state.chain.chainId,
       };
-      const _res = await axios.post(`/api/transaction/${props.transactionID}/signature`, signature);
-      props.addSignature(signature);
-      setHasSigned(true);
+
+      const { bodyBytes, signatures } = await signingClient.sign(
+        signerAddress,
+        props.tx.msgs,
+        props.tx.fee,
+        props.tx.memo,
+        signerData,
+      );
+
+      // check existing signatures
+      const bases64EncodedSignature = toBase64(signatures[0]);
+      const bases64EncodedBodyBytes = toBase64(bodyBytes);
+      const prevSigMatch = props.signatures.findIndex(
+        (signature) => signature.signature === bases64EncodedSignature,
+      );
+
+      if (prevSigMatch > -1) {
+        setSigError("This account has already signed.");
+      } else {
+        const signature = {
+          bodyBytes: bases64EncodedBodyBytes,
+          signature: bases64EncodedSignature,
+          address: signerAddress,
+        };
+        const _res = await axios.post(
+          `/api/transaction/${props.transactionID}/signature`,
+          signature,
+        );
+        props.addSignature(signature);
+        setHasSigned(true);
+      }
+    } catch (e) {
+      console.log("signing err: ", e);
+    } finally {
+      setLoading((newLoading) => ({ ...newLoading, signing: false }));
     }
   };
 
@@ -144,12 +172,20 @@ const TransactionSigning = (props: Props) => {
                   Connected signer {walletAccount.bech32Address} (
                   {walletType ?? "Unknown wallet type"}).
                 </p>
-                <Button label="Sign transaction" onClick={signTransaction} />
+                <Button
+                  label="Sign transaction"
+                  onClick={signTransaction}
+                  loading={loading.signing}
+                />
               </>
             ) : (
               <>
-                <Button label="Connect Keplr" onClick={connectKeplr} />
-                <Button label="Connect Ledger (WebUSB)" onClick={connectLedger} />
+                <Button label="Connect Keplr" onClick={connectKeplr} loading={loading.keplr} />
+                <Button
+                  label="Connect Ledger (WebUSB)"
+                  onClick={connectLedger}
+                  loading={loading.ledger}
+                />
               </>
             )}
           </StackableContainer>
