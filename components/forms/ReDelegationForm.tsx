c7238ae6bd6dd97978c04158cb48a062aa1ba7d8
abefernan <44572727+abefernan@users.noreply.github.com>
2023-04-24 22:42:37 +0200

    Remove null typing for account on forms

--- components/forms/ReDelegationForm.tsx
+++ components/forms/ReDelegationForm.tsx
@@ -12,7 +12,7 @@ import StackableContainer from "../layout/StackableContainer";
 
 interface Props {
   delegatorAddress: string;
-  accountOnChain: Account | null;
+  accountOnChain: Account;
   router: NextRouter;
   closeForm: () => void;
 }
@@ -56,7 +56,6 @@ const ReDelegationForm = (props: Props) => {
     assert(gasPrice, "gasPrice missing");
     const fee = calculateFee(gasLimit, gasPrice);
     const { accountOnChain } = props;
-    assert(accountOnChain, "accountOnChain missing");
     assert(typeof accountOnChain.accountNumber === "number", "accountNumber missing");
     return {
       accountNumber: accountOnChain.accountNumber,
--- components/forms/TransactionForm.tsx
+++ components/forms/TransactionForm.tsx
@@ -12,7 +12,7 @@ import StackableContainer from "../layout/StackableContainer";
 
 interface Props {
   address: string | null;
-  accountOnChain: Account | null;
+  accountOnChain: Account;
   router: NextRouter;
   closeForm: () => void;
 }
@@ -51,7 +51,6 @@ const TransactionForm = (props: Props) => {
     assert(gasPrice, "gasPrice missing");
     const fee = calculateFee(gasLimit, gasPrice);
     const { accountOnChain } = props;
-    assert(accountOnChain, "accountOnChain missing");
     assert(typeof accountOnChain.accountNumber === "number", "accountNumber missing");
     return {
       accountNumber: accountOnChain.accountNumber,
