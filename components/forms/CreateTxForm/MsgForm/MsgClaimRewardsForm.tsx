b8501be6747b0343f40749a2be3d4f9c4ac9515d
abefernan <44572727+abefernan@users.noreply.github.com>
2023-05-05 13:25:45 +0200

    Use msgGetter and deleteMsg in MsgClaimRewardsForm

--- components/forms/CreateTxForm/MsgForm/MsgClaimRewardsForm.tsx
+++ components/forms/CreateTxForm/MsgForm/MsgClaimRewardsForm.tsx
@@ -1,47 +1,69 @@
 import { assert } from "@cosmjs/utils";
-import { Dispatch, SetStateAction, useCallback, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
+import { MsgGetter } from "..";
 import { useAppContext } from "../../../../context/AppContext";
 import { checkAddress, exampleAddress } from "../../../../lib/displayHelpers";
+import { isTxMsgClaimRewards } from "../../../../lib/txMsgHelpers";
 import { TxMsg, TxMsgClaimRewards } from "../../../../types/txMsg";
 import Input from "../../../inputs/Input";
+import StackableContainer from "../../../layout/StackableContainer";
 
 interface MsgClaimRewardsFormProps {
   readonly delegatorAddress: string;
-  readonly setCheckAndGetMsg: Dispatch<SetStateAction<(() => TxMsg | null) | undefined>>;
+  readonly setMsgGetter: (msgGetter: MsgGetter) => void;
+  readonly deleteMsg: () => void;
 }
 
-const MsgClaimRewardsForm = ({ delegatorAddress, setCheckAndGetMsg }: MsgClaimRewardsFormProps) => {
+const MsgClaimRewardsForm = ({
+  delegatorAddress,
+  setMsgGetter,
+  deleteMsg,
+}: MsgClaimRewardsFormProps) => {
   const { state } = useAppContext();
   assert(state.chain.addressPrefix, "addressPrefix missing");
 
   const [validatorAddress, setValidatorAddress] = useState("");
   const [validatorAddressError, setValidatorAddressError] = useState("");
 
-  const checkAndGetMsg = useCallback(() => {
-    assert(state.chain.addressPrefix, "addressPrefix missing");
-
-    const addressErrorMsg = checkAddress(validatorAddress, state.chain.addressPrefix);
-    if (addressErrorMsg) {
-      setValidatorAddressError(
-        `Invalid address for network ${state.chain.chainId}: ${addressErrorMsg}`,
-      );
-      return null;
-    }
-
-    const msg: TxMsgClaimRewards = {
-      typeUrl: "/cosmos.distribution.v1beta1.MsgWithdrawDelegatorReward",
-      value: { delegatorAddress, validatorAddress },
-    };
-
-    return msg;
-  }, [delegatorAddress, state.chain.addressPrefix, state.chain.chainId, validatorAddress]);
-
   useEffect(() => {
-    setCheckAndGetMsg(() => checkAndGetMsg);
-  }, [checkAndGetMsg, setCheckAndGetMsg]);
+    try {
+      setValidatorAddressError("");
+
+      const isMsgValid = (msg: TxMsg): msg is TxMsgClaimRewards => {
+        assert(state.chain.addressPrefix, "addressPrefix missing");
+
+        const addressErrorMsg = checkAddress(validatorAddress, state.chain.addressPrefix);
+        if (addressErrorMsg) {
+          setValidatorAddressError(
+            `Invalid address for network ${state.chain.chainId}: ${addressErrorMsg}`,
+          );
+          return false;
+        }
+
+        return isTxMsgClaimRewards(msg);
+      };
+
+      const msg: TxMsgClaimRewards = {
+        typeUrl: "/cosmos.distribution.v1beta1.MsgWithdrawDelegatorReward",
+        value: { delegatorAddress, validatorAddress },
+      };
+
+      setMsgGetter({ isMsgValid, msg });
+    } catch {}
+  }, [
+    delegatorAddress,
+    setMsgGetter,
+    state.chain.addressPrefix,
+    state.chain.chainId,
+    validatorAddress,
+  ]);
 
   return (
-    <>
+    <StackableContainer lessPadding lessMargin>
+      <button className="remove" onClick={() => deleteMsg()}>
+        ✕
+      </button>
+      <h2>MsgWithdrawDelegatorReward</h2>
       <div className="form-item">
         <Input
           label="Validator Address"
@@ -53,14 +75,22 @@ const MsgClaimRewardsForm = ({ delegatorAddress, setCheckAndGetMsg }: MsgClaimRe
         />
       </div>
       <style jsx>{`
-        p {
-          margin-top: 15px;
-        }
         .form-item {
           margin-top: 1.5em;
         }
+        button.remove {
+          background: rgba(255, 255, 255, 0.2);
+          width: 30px;
+          height: 30px;
+          border-radius: 50%;
+          border: none;
+          color: white;
+          position: absolute;
+          right: 10px;
+          top: 10px;
+        }
       `}</style>
-    </>
+    </StackableContainer>
   );
 };
 
