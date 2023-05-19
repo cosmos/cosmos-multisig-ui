cf7d9edba9fde16785c3199b9a2d29af5b61468a
abefernan <44572727+abefernan@users.noreply.github.com>
2023-05-19 13:43:02 +0200

    Add back button to create multisig

--- pages/create.tsx
+++ pages/create.tsx
@@ -1,11 +1,13 @@
-import React from "react";
-
+import Link from "next/link";
 import MultisigForm from "../components/forms/MultisigForm";
 import Page from "../components/layout/Page";
 import StackableContainer from "../components/layout/StackableContainer";
 
 const CreatePage = () => (
   <Page>
+    <Link href="/" style={{ color: "white", fontSize: 20, textDecoration: "none" }}>
+      ← Back to Home
+    </Link>
     <StackableContainer base>
       <StackableContainer lessPadding>
         <h1 className="title">Create Legacy Multisig</h1>
