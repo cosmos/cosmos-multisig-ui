649c1a7b98e5f98f4f76c1d9f28d8e3085dde8a2
abefernan <44572727+abefernan@users.noreply.github.com>
2023-07-21 19:51:17 +0200

    Add requestJson

---
+++ lib/request.ts
@@ -0,0 +1,16 @@
+export type RequestConfig = Omit<RequestInit, "body"> & { body?: unknown };
+
+export const requestJson = async (
+  endpoint: string,
+  { method, headers, body, ...restConfig }: RequestConfig = {},
+) => {
+  const config: RequestInit = {
+    method: method ?? body ? "POST" : "GET",
+    headers: { "Content-Type": "application/json", ...headers },
+    body: body ? JSON.stringify(body) : null,
+    ...restConfig,
+  };
+
+  const response = await fetch(endpoint, config);
+  return response.ok ? response.json() : Promise.reject(new Error(await response.text()));
+};
