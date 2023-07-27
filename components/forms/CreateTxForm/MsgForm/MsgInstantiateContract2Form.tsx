da8e704f3e5a2642c8954ddbd1e919a641f15c47
abefernan <44572727+abefernan@users.noreply.github.com>
2023-07-27 09:48:08 +0200

    Conditionally render custom denom input

--- components/forms/CreateTxForm/MsgForm/MsgInstantiateContract2Form.tsx
+++ components/forms/CreateTxForm/MsgForm/MsgInstantiateContract2Form.tsx
@@ -244,21 +244,23 @@ const MsgInstantiateContract2Form = ({
           }}
         />
       </div>
-      <div className="form-item">
-        <Input
-          label="Custom denom"
-          name="custom-denom"
-          value={customDenom}
-          onChange={({ target }) => setCustomDenom(target.value)}
-          placeholder={
-            selectedDenom.value === customDenomOption.value
-              ? "Enter custom denom"
-              : "Select Custom denom above"
-          }
-          disabled={selectedDenom.value !== customDenomOption.value}
-          error={customDenomError}
-        />
-      </div>
+      {selectedDenom.value === customDenomOption.value ? (
+        <div className="form-item">
+          <Input
+            label="Custom denom"
+            name="custom-denom"
+            value={customDenom}
+            onChange={({ target }) => setCustomDenom(target.value)}
+            placeholder={
+              selectedDenom.value === customDenomOption.value
+                ? "Enter custom denom"
+                : "Select Custom denom above"
+            }
+            disabled={selectedDenom.value !== customDenomOption.value}
+            error={customDenomError}
+          />
+        </div>
+      ) : null}
       <div className="form-item">
         <Input
           type="number"
