69e75323bae6140a8dad17b32dbed687513a6c4d
abefernan <44572727+abefernan@users.noreply.github.com>
2023-09-11 17:23:21 +0200

    Add ThemeProvider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,3 +1,5 @@
+import ThemeProvider from "@/context/ThemesContext";
+import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import ChainSelect from "../components/chainSelect/ChainSelect";
 import { ChainsProvider } from "../context/ChainsContext";
@@ -6,8 +8,12 @@ function MultisigApp({ Component, pageProps }: AppProps) {
   const showChainSelect = process.env.NEXT_PUBLIC_MULTICHAIN?.toLowerCase() === "true";
   return (
     <ChainsProvider>
+      <ThemeProvider attribute="class" defaultTheme="dark" enableSystem>
+        {showChainSelect && <ChainSelect />}
+        <Component {...pageProps} />
       {showChainSelect && <ChainSelect />}
       <Component {...pageProps} />
+      </ThemeProvider>
     </ChainsProvider>
   );
 }
