a45f0603460cfa11ab33292d4653ecfbc0bb9812
abefernan <44572727+abefernan@users.noreply.github.com>
2023-12-01 16:03:38 +0100

    Move /create to /chainName/create

---
+++ pages/[chainName]/create.tsx
@@ -0,0 +1,19 @@
+import MultisigForm from "@/components/forms/MultisigForm";
+import Page from "@/components/layout/Page";
+import StackableContainer from "@/components/layout/StackableContainer";
+import { useChains } from "@/context/ChainsContext";
+
+export default function CreatePage() {
+  const { chain } = useChains();
+
+  return (
+    <Page goBack={{ pathname: `/${chain.registryName}`, title: "home", needsConfirm: true }}>
+      <StackableContainer base>
+        <StackableContainer lessPadding>
+          <h1 className="title">Create Legacy Multisig</h1>
+        </StackableContainer>
+        <MultisigForm />
+      </StackableContainer>
+    </Page>
+  );
+}
