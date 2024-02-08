bee9df022f95a6fdfb2372d29e553406a35cc3a6
Simon Warta <simon@warta.it>
2024-02-08 17:06:06 +0100

    Elaborate in Warning message

--- components/MigrationWarning.tsx
+++ components/MigrationWarning.tsx
@@ -5,17 +5,19 @@ export default function MigrationWarning() {
     <Alert className="mx-auto my-4 max-w-md border-yellow-300 bg-fuchsia-900">
       <AlertTitle>Warning!</AlertTitle>
       <AlertDescription className="mt-3">
-        This app will no longer be found at this url from March the 1st.
+        This app will no longer be available at this URL from March 1st, 2024.
       </AlertDescription>
       <AlertDescription className="mt-3">
-        It will instead be accessible from{" "}
+        A new instance is deployed at{" "}
         <a href="https://multisig.confio.run" target="_blank" className="text-blue-400">
           https://multisig.confio.run
         </a>
-        .
+        , connecting to a new database. You can start using this instance for new signing requests.
+        Re-importing multisigs by address or public keys may be needed.
       </AlertDescription>
       <AlertDescription className="mt-3">
-        Any pending transactions on this app will be lost when that time arrives.
+        Any pending transactions on this app will be lost when that time arrives. In the meantime,
+        both deployments can be used but no data is shared between them.
       </AlertDescription>
     </Alert>
   );
