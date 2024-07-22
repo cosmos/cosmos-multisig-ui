ab3aedbb68acd5c32290c45197cef1aa10115ae0
abefernan <44572727+abefernan@users.noreply.github.com>
2024-07-22 12:38:03 +0200

    Add amount field and schema

---
+++ components/forms/CreateTxForm/Fields/FieldAmount.tsx
@@ -0,0 +1,49 @@
+import { FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { prettyFieldName } from "@/lib/form";
+import * as z from "zod";
+import type { FieldProps } from "./types";
+
+export const getFieldAmountSchema = () =>
+  z.object({
+    denom: z
+      .string({ invalid_type_error: "Must be a string", required_error: "Required" })
+      .trim()
+      .min(1, "Required"),
+    amount: z
+      .number({ invalid_type_error: "Must be a number", required_error: "Required" })
+      .positive("Must be positive"),
+  });
+
+export default function FieldAmount({ form, fieldFormName }: FieldProps) {
+  return (
+    <>
+      <FormField
+        control={form.control}
+        name={`${fieldFormName}.denom`}
+        render={({ field }) => (
+          <FormItem>
+            <FormLabel>{"Denom" + prettyFieldName(fieldFormName)}</FormLabel>
+            <FormControl>
+              <Input placeholder="Enter denom" {...field} />
+            </FormControl>
+            <FormMessage />
+          </FormItem>
+        )}
+      />
+      <FormField
+        control={form.control}
+        name={`${fieldFormName}.amount`}
+        render={({ field }) => (
+          <FormItem>
+            <FormLabel>{prettyFieldName(fieldFormName)}</FormLabel>
+            <FormControl>
+              <Input placeholder="Enter amount" {...field} />
+            </FormControl>
+            <FormMessage />
+          </FormItem>
+        )}
+      />
+    </>
+  );
+}
