a054b740ee69c6ebbc3ac706b6ee6eb2117bf4c6
abefernan <44572727+abefernan@users.noreply.github.com>
2024-07-31 13:27:24 +0200

    Add FieldCommission

--- components/forms/CreateTxForm/Fields/index.ts
+++ components/forms/CreateTxForm/Fields/index.ts
@@ -1,6 +1,7 @@
 export * from "./FieldAddress";
 export * from "./FieldAmount";
 export * from "./FieldBoolean";
+export * from "./FieldCommission";
 export * from "./FieldDescription";
 export * from "./FieldNumber";
 export * from "./FieldString";
