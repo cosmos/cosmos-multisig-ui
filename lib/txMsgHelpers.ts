bbcb0d54321f977321fe59c991c9ec9da9a9b59b
Simon Warta <simon@warta.it>
2024-12-10 16:38:08 +0100

    Increase WithdrawDelegatorReward gas to 500k

--- lib/txMsgHelpers.ts
+++ lib/txMsgHelpers.ts
@@ -22,7 +22,9 @@ const gasOfMsg = (msgType: MsgTypeUrl): number => {
     case MsgTypeUrls.SetWithdrawAddress:
       return 100_000;
     case MsgTypeUrls.WithdrawDelegatorReward:
-      return 100_000;
+      // On the Hub we now claim so many coins at once that this operation can become gas expensive.
+      // See e.g. https://www.mintscan.io/cosmos/tx/EA7EC3F6F08DA4E6D419359F264B34AB27D2AAE7FF40267E7E760927475157B3
+      return 500_000;
     // Vesting
     case MsgTypeUrls.CreateVestingAccount:
       return 100_000;
